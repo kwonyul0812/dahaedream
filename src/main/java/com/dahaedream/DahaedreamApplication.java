3cc8df01010c4be9b33a694f78f60d7caa8c98c1
kwonyul <kw990812@gmail.com>
2025-04-03 22:48:58 +0900

    테스트 커밋

---
+++ src/main/java/com/dahaedream/DahaedreamApplication.java
@@ -0,0 +1,13 @@
+package com.dahaedream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DahaedreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DahaedreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dahaedream/DahaedreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahaedream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DahaedreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
