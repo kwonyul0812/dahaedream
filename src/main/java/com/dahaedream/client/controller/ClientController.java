2546f99c3e9654d79a3300368e2e6c19f6173c19
codingjs12 <202245068@itc.ac.kr>
2025-04-08 20:52:58 +0900

    250408 의뢰목록, 의뢰요청 수락 기능

--- src/main/java/com/dahaedream/client/controller/ClientController.java
+++ src/main/java/com/dahaedream/client/controller/ClientController.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ClientController {
 
-    @GetMapping("/client")
-    public String client() {
+    @GetMapping("/client/add")
+    public String clientAdd() {
         return "/client/client-add";
     }
+
+    @GetMapping("/client/request")
+    public String clientRequest() {
+        return "/client/client-request";
+    }
+
+    @GetMapping("/client/list")
+    public String client() {
+        return "/client/client-list";
+    }
 }
+
