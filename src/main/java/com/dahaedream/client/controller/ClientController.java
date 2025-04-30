84c358a24dec21f03ab7ce74770ecf8fc64d496e
codingjs12 <202245068@itc.ac.kr>
2025-04-30 20:21:57 +0900

    250430 매퍼 이동 리턴 주소 / 제거

--- src/main/java/com/dahaedream/client/controller/ClientController.java
+++ src/main/java/com/dahaedream/client/controller/ClientController.java
@@ -19,21 +19,21 @@ public class ClientController {
 
     @GetMapping("/client/add")
     public String clientAdd() {
-        return "/client/client-add";
+        return "client/client-add";
     }
 
     @GetMapping("/client/request")
     public String clientRequest() {
-        return "/client/client-request";
+        return "client/client-request";
     }
 
     @GetMapping("/client/list")
     public String clientInfo() {
-        return "/client/client-list";
+        return "client/client-list";
     }
     @GetMapping("/client/info")
     public String client() {
-        return "/client/client-info"; 
+        return "client/client-info";
     }
 
     @PostMapping("/client/insert.dox")
